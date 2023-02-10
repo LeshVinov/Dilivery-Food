ec63e2a419cf5a47ad4d3874fb692afe5b2c0d2d
LeshVinov <90595946+LeshVinov@users.noreply.github.com>
2023-02-10 12:17:19 +0300

    feat: modal window done finally

--- js/script.js
+++ js/script.js
@@ -1,65 +1,129 @@
-const modal = document.querySelector('.modal');
-const buttonCart = document.querySelector('.button__cart');
-const buttonClosed = document.querySelector('.button-closed');
-const cancelButton = document.getElementById('cancelButton');
-const countSpan = document.querySelector('.count');
-const countBlocks = document.querySelectorAll('.count-block')
-const fullPrice =  document.querySelector('.fullprice');
+// const modal = document.querySelector('.modal');
+// const buttonCart = document.querySelector('.button__cart');
+// const buttonClosed = document.querySelector('.button-closed');
+// const cancelButton = document.getElementById('cancelButton');
+// const countSpan = document.querySelector('.count');
+// const fullPrice =  document.querySelector('.fullprice');
+// const countBlock = document.querySelectorAll('.count-block');
 
-let countMinButton;
-let countPlusButton;
-
-
-
-const toggleModal = function() {
-    modal.classList.toggle('open');
-}
-
-const removeOfModal = function(event) {
-    if (event.target.classList === modal.classList) {
-        modal.classList.remove('open')
-    }
-}
-
-buttonCart.addEventListener('click', toggleModal);
-buttonClosed.addEventListener('click', toggleModal);
-cancelButton.addEventListener('click', toggleModal);
-modal.addEventListener('click', removeOfModal);
-
-for (let i = 0; i <countBlocks.Length; i++){
-    countMinButton = countBlock.querySelector('.count-min');
-    countPlusButton = countBlock.querySelector('.count-min');
-    let countSpan = countBlock.querySelector('.count');
-    const counterMinus = function () {
-        if(+countSpan.textContent === 0) {
-            countSpan.textContent = 0
-        } else {
-            count = +countSpan.textContent;
-            count--
-            countSpan.textContent = count;
-        }
-    };
-    const counterPlus = function () {
-        count = +countSpan.textContent;
-        count++
-        countSpan.textContent = count;
-    };
-
-    countMinButton.addEventListener('click', counterMinus);
-    countPlusButton.addEventListener('click', counterPlus);
-}
-
-countMinButton.forEach(function(countMinButton) {
-    countMinButton.addEventListener('click', counterMinus);
-})
-
-countPlusButton.forEach(function(countPlusButton) {
-    countPlusButton.addEventListener('click', counterPlus);
-})
+// const countMinButton = document.querySelectorAll('.button-count--min')
+// const countPlusButton = document.querySelectorAll('.button-count--plus')
 
 
-console.log (typeof +countSpan.textContent)
+
+
+// const toggleModal = function() {
+//     modal.classList.toggle('open');
+// }
+
+// const removeOfModal = function(event) {
+//     if (event.target.classList === modal.classList) {
+//         modal.classList.remove('open')
+//     }
+// }
+
+// buttonCart.addEventListener('click', toggleModal);
+// buttonClosed.addEventListener('click', toggleModal);
+// cancelButton.addEventListener('click', toggleModal);
+// modal.addEventListener('click', removeOfModal);
+
+// countBlock.forEach(countBlock => {
+//     console.log(countBlock.countSpan);
+// })
+
+// countMinButton.forEach(countMinButton => {
+//     countMinButton.addEventListener('click', () => {
+//         if (countSpan.textContent > 1) {
+//             let count
+//             count = countSpan.textContent
+//             count--
+//             countSpan.textContent = count
+//         }
+//     })
+// });
+// countPlusButton.forEach(countPlusButton => {
+//     countPlusButton.addEventListener('click', () => {
+//         let count
+//         count = countSpan.textContent
+//         count++
+//         countSpan.textContent = count
+//     })
+// })
+
+// console.log(fullPrice.textContent);
+
+// console.log (+countSpan.textContent)
 // console.log(modal);
 // console.log(buttonCart);
 // console.log(buttonClosed);
 //  console.log(cancelButton);\
+
+const modal = document.querySelector('.modal');
+const btnCart = document.querySelector('.button__cart');
+const btnClose = document.querySelector('.button-closed');
+const btnCancel = document.getElementById('cancelButton');
+const rows = modal.querySelectorAll('.cart-string');
+const total = modal.querySelector('.fullprice');
+
+btnCart.addEventListener('click', toggleModal);
+btnClose.addEventListener('click', toggleModal);
+btnCancel.addEventListener('click', toggleModal);
+
+
+
+function toggleModal() {
+	modal.classList.toggle('open');
+};
+
+modal.addEventListener('click', (event)=>{
+	if(event.target == modal){
+		toggleModal()
+	}
+})
+
+
+
+const getFullPrice = () => {
+	let fullPrice = 0;
+	const rows = modal.querySelectorAll('.cart-string');
+	rows.forEach(row => {
+		let priceBlock = row.querySelector('.price-dish')
+		let price = +priceBlock.textContent 
+		fullPrice += price;
+	})
+	total.textContent = fullPrice
+}
+
+const getNewPrice = (count, price, priceBlock) => {
+	newPrice = count * price
+	priceBlock.textContent = newPrice
+	getFullPrice()
+}
+
+rows.forEach(row => {
+	let priceBlock = row.querySelector('.price-dish')
+	let price = +priceBlock.textContent 
+	let countBlock = row.querySelector('.count')
+	let count = +countBlock.textContent
+	const btnMinus = row.querySelector('.button-count--min')
+	const btnPlus = row.querySelector('.button-count--plus')
+
+
+	
+	btnMinus.addEventListener('click', () => {
+		if (count > 0) {
+			count--
+			countBlock.textContent = count
+			getNewPrice(count, price, priceBlock)
+		}
+	})
+
+	btnPlus.addEventListener('click', () => {
+		count++
+		countBlock.textContent = count
+		getNewPrice(count, price, priceBlock)
+	})
+	
+})
+
+getFullPrice()
